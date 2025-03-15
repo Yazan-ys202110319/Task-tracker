e9b52d613cb8faa6eb52a19d2a583cdee14cb8c7
Yazan Jumaa <ys2110319@qu.edu.qa>
2025-03-15 22:19:27 +0300

    Refactor task management to use localStorage and enhance task rendering with complete and delete functionality

--- script.js
+++ script.js
@@ -1,40 +1,101 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // An array of tasks and each task is an object 
-    // This is the state of the application
-    const tasks = [
-        {title: "Task 1", complete: true},
-        {title: "Task 2", complete: false},
-        {title: "Task 3", complete: true}
-    ]
-
+    let tasks = localStorage.getItem("tasks")
+      ? JSON.parse(localStorage.getItem("tasks"))
+      : [];
+      
     function renderTask(task) {
-        // Here it will create an element which is a div and returns it and you can have full control of the element.
-        // There is 3 childs for the task div which are task title, complete button and delete button.
-
-        const titleDiv = document.createElement("div");
-        titleDiv.classList.add("task-title");
-        titleDiv.innerHTML = task.title;
-
-        
-        const completeButton = document.createElement("button");
-
-
-        const deleteButton = document.createElement("button");
-
-
-
-        // Because each task has under it a complete button and delete button and title
-
-
-
-        // taskDiv.appendChild(titleDiv);
-        // taskDiv.appendChild(completeButton);
-        // taskDiv.appendChild(deleteButton);
-
-        return titleDiv;
-
+      const taskDiv = document.createElement("div");
+      taskDiv.classList.add("task");
+      if (task.complete) {
+        taskDiv.classList.add("complete");
+      }
+  
+      const titleDiv = document.createElement("div");
+      titleDiv.classList.add("task-title");
+      titleDiv.innerHTML = task.title;
+  
+      const completeButton = document.createElement("button");
+      const completeImage = document.createElement("img");
+      completeImage.src = `icons/${task.complete ? "check-" : ""}square.svg`;
+      completeImage.alt = "un/complete task";
+      completeButton.appendChild(completeImage);
+      completeButton.addEventListener("click", function () {
+        completeTask(task.title);
+      });
+  
+      // const completeCheck = document.createElement("input");
+      // completeCheck.type = "checkbox";
+      // completeCheck.checked = task.complete;
+      // completeCheck.addEventListener("click", function () {
+      //   completeTask(task.title);
+      // });
+  
+      const deleteButton = document.createElement("button");
+      deleteButton.disabled = !task.complete;
+      const deleteImage = document.createElement("img");
+      deleteImage.src = `icons/x-circle.svg`;
+      deleteImage.alt = "delete task";
+      deleteButton.appendChild(deleteImage);
+      deleteButton.addEventListener("click", function () {
+        deleteTask(task.title);
+      });
+  
+      taskDiv.appendChild(titleDiv);
+      taskDiv.appendChild(completeButton);
+      // taskDiv.appendChild(completeCheck);
+      taskDiv.appendChild(deleteButton);
+      return taskDiv;
     }
-
-    console.log(renderTask(tasks[0]));
-
-});
+  
+    function renderTasks() {
+      const container = document.querySelector("#tasks");
+      // container.innerHTML = null;
+      container.replaceChildren();
+      tasks.forEach((task) => container.appendChild(renderTask(task)));
+  
+      if (tasks.length) {
+        document.querySelector("#task-reset").classList.remove("hidden");
+      } else {
+        document.querySelector("#task-reset").classList.add("hidden");
+      }
+  
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+  
+    function addTask() {
+      const titleInput = document.querySelector("#new-task-title");
+      const title = titleInput.value.trim();
+      if (title && !tasks.find((task) => task.title === title)) {
+        tasks.push({
+          title,
+          complete: false,
+        });
+        titleInput.value = "";
+        renderTasks();
+      }
+    }
+  
+    function clearTasks() {
+      tasks = [];
+      renderTasks();
+    }
+  
+    function completeTask(title) {
+      const task = tasks.find((task) => task.title === title);
+      task.complete = !task.complete;
+      renderTasks();
+    }
+  
+    function deleteTask(title) {
+      // const index = tasks.findIndex((task) => task.title === title);
+      // tasks.splice(index, 1);
+      tasks = tasks.filter((task) => task.title !== title);
+      renderTasks();
+    }
+  
+    document.querySelector("#add-task").addEventListener("click", addTask);
+    document.querySelector("#clear-tasks").addEventListener("click", clearTasks);
+  
+    renderTasks();
+  });
+  
